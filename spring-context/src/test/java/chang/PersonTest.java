97f5362e7f687ab7bb06abcba765676b7ef4d1e8
火神分身 <guichang2010@163.com>
2021-04-15 00:59:43 +0800

    增加自测的Person

---
+++ spring-context/src/test/java/chang/PersonTest.java
@@ -0,0 +1,20 @@
+package chang;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * person测试
+ *
+ * @author guichang
+ * @date 2021/4/15
+ */
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:chang/person.xml");
+		Person person = (Person) context.getBean("person");
+		System.out.println(person);
+	}
+
+}
