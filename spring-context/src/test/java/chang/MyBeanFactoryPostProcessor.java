1d207ec4ecbdcef0fe2e1682f858760189eb4e35
火神分身 <guichang2010@163.com>
2021-04-19 00:44:21 +0800

    invokeBeanFactoryPostProcessors

---
+++ spring-context/src/test/java/chang/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,20 @@
+package chang;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * 暂无描述
+ *
+ * @author guichang
+ * @date 2021/4/18
+ */
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		System.out.println("====== MyBeanFactoryPostProcessor ======");
+	}
+}
